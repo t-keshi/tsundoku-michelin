923014bccc8892937bcd433773949b3171b68de8
t-keshi <quick.resp.biz094@gmail.com>
2022-07-22 00:35:38 +0900

    fix: deploy

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import { byText } from 'testing-library-selector';
 import { render } from '../src/testUtils';
-import Home from '../src/pages/index';
+import Home from '../src/page_bk/index';
 import '@testing-library/jest-dom/extend-expect';
 
 const ui = {
